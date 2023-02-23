15344e890480aba17ca0288a4e0dd6afcbe9ebd3
Jwell2014 <91597225+Jwell2014@users.noreply.github.com>
2023-02-23 18:34:23 +0100

    animation et css card

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,16 @@
-import React, {FunctionComponent, useEffect, useState } from 'react';
+import React, {FunctionComponent } from 'react';
 import "./styles/App.css";
-import Pokemon from './models/pokemon';
-import listPokemon from './models/listPokemon'
+import PokemonList from './pages/pokemon-list';
 
 
 const App: FunctionComponent = () => {
-  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-  
-
-  useEffect( () => {
-    setPokemons(listPokemon)
-  }, [] )
 
   return(
-    <div>
-      <h1 className='center'>POKÉDEX</h1>
-      <div className='container'>
-        <div className='row'>
-          {pokemons.map(({id, name, picture, created}) =>
-          (<div className='col s6 m4' key={id}>
-            <div className='card horizontal'> 
-              <div className='card-image'>
-                <img src={picture} alt={name}/>
-              </div> 
-              <div className='card-started'>
-                <div className='card-content'>
-                  <p>{name}</p>
-                  <p><small>{created.toString()}</small></p>
-                </div>
-              </div>
-            </div>
-          </div>)
-          )}
-        </div>
-      </div>
-    </div>
+    
+          <>
+          <PokemonList />
+          </>
+        
   )
 }
 
---
+++ src/pages/pokemon-list.tsx
@@ -0,0 +1,28 @@
+import React, {FunctionComponent, useState,useEffect} from "react";
+import Pokemon from '../models/pokemon';
+import POKEMONS from "../models/listPokemon";
+import PokemonCard from "../components/pokemon-card";
+
+
+const PokemonList: FunctionComponent = () => {
+    const [pokemons, setPokemons] = useState<Pokemon[]>([])
+
+    useEffect(()=> {
+        setPokemons(POKEMONS);
+    },[])
+    return (<div>
+        <h1 className='center'>POKÉDEX</h1>
+        <div className='container'>
+          <div className='row'>
+          {pokemons.map((pokemon) =>
+          (<PokemonCard key={pokemon.id} pokemon={pokemon}/>
+          ))}
+          </div>
+      </div>
+    </div>
+
+    )
+}
+
+
+export default PokemonList;
